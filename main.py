b848dea11ff6e4ab5a1890e235fd5092e39e67eb
Denzel <114736457+denzelesh@users.noreply.github.com>
2023-01-21 16:46:50 +0000

    Created Regular Blocks

--- main.py
+++ main.py
@@ -1,73 +1,20 @@
 import turtle   #used to create turtle objects
+import time     #used to delay execution of code
+
 
 
 ### Initialising Constants
 gameplay = True
 wait_before_reloop = 0.15
-
-### Screen Setup
-game_screen = turtle.Screen()   # Creates a new turtle object for the game screen
-# Below defines the characteristics of the game screen
-game_screen_size = 626
-game_screen.setup(width=game_screen_size, height=game_screen_size)  # Creates a perfectly square game screen
-game_screen.bgcolor("purple")
-
-# Main Game Loop
-
-while gameplay == True:
-    game_screen.update()    # Updates Screen With Changes
-
-  
-
-import turtle   #used to create turtle objects
-
-
-
-
-### Initialising Constants
-gameplay = True
-
-pause_until_input = "stop"  #prevents object from initially moving without user input
-object_speed = 0    # this is the animation speed of the objects
-
-
-
-### Screen Setup
-game_screen = turtle.Screen()   # Creates a new turtle object for the game screen
-# Below defines the characteristics of the game screen
-game_screen_size = 626
-game_screen.setup(width=game_screen_size, height=game_screen_size)  # Creates a perfectly square game screen
-game_screen.bgcolor("purple")
-
-
-
-### Mamba Setup
-mamba_head = turtle.Turtle()    # Creates a new turtle object for the mamba head
-# Below defines the characteristics of the mamba head
-mamba_head.shape("square")
-mamba_start_postiion = (0,0)    #will center the position of the mamba head
-mamba_head.direction = pause_until_input
-mamba_head.speed(object_speed)
-mamba_head.goto(mamba_start_postiion)
-mamba_head.penup()  # This prevents the mamba head from drawing anything as it moves, ie. leaving no trace
-
-
-# Main Game Loop
-
-while gameplay == True:
-    game_screen.update()    # Updates Screen With Changes
-    
-    
-import turtle   #used to create turtle objects
-
-
-
-### Initialising Constants
-gameplay = True
 pause_until_input = "stop"  #prevents object from initially moving without user input
 default_object_size = 20
-object_speed = 0    # this is the animation speed of the objects
 
+
+
+
+## Intial Values For Variables
+
+object_speed = 0    # this is the animation speed of the objects
 
 
 ### Screen Setup
@@ -91,9 +38,19 @@ mamba_head.goto(mamba_start_postiion)
 mamba_head.penup()  # This prevents the mamba head from drawing anything as it moves, ie. leaving no trace
 
 
+### Regular Block Setup
+regular_block = turtle.Turtle() # Creates a new turtle object for the regular blocks
+# Below defines the characteristics of the regular block
+regular_block.shape("circle")
+regular_block.speed(object_speed)
+regular_block.penup()   # This prevents the regular block from drawing anything as it moves, ie. leaving no trace
+regular_block.color("red")
+
+
 ## Keyboard Functions (to Move mamba)
 #### the != statments are used to provide self impact due to the mabma head reversing into extentions
 ### to be created later.
+
 def turn_left():
     if mamba_head.direction != "right":
         mamba_head.direction = "left"
@@ -144,9 +101,3 @@ while gameplay == True:
 
 
     time.sleep(wait_before_reloop)  #make the loop wait before re-looping to prevent the mamba object from moving too fast
-
-
-
-
-
-
